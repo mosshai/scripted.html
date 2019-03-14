8809df6e2d17d38789656b44410faae2af9b5bfa
Koushik Dutta <koushd@gmail.com>
2019-03-14 16:53:29 -0700

    update

--- scrypted/index.js
+++ scrypted/index.js
@@ -1,10 +1,23 @@
 const fs = require('fs');
-var swig  = require('swig');
-var template = swig.compileFile('./index.html.md.j2');
+// var swig  = require('swig');
+// var template = swig.compileFile('./index.html.md.j2');
+var template = fs.readFileSync('./index.html.md.j2').toString();
 var json = JSON.parse(fs.readFileSync('./model.json'));
 
-var output = template({
+for (var iface in json) {
+    var filename = `../source/includes/scrypted/_${iface}.md.erb`;
+    if (!fs.existsSync(filename)) {
+        fs.writeFileSync(filename, '');
+    }
+}
+
+const nunjucks = require('nunjucks');
+nunjucks.configure({ autoescape: false });
+var output = nunjucks.renderString(template, {
     classes: json,
+    methodArguments: function(args) {
+        return args.map(arg => `${arg.name}: ${arg.type}`);
+    }
 });
 
-fs.writeFileSync('../source/index.html.md', output);
+fs.writeFileSync('../source/index.html.md.erb', output);
