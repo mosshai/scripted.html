6d7fa78fd9f90bbdfef7797a71386479cadd29e1
Koushik Dutta <koushd@gmail.com>
2019-06-17 19:08:28 -0700

    update

--- lib/multilang.rb
+++ lib/multilang.rb
@@ -1,6 +1,30 @@
 module Multilang
   def block_code(code, full_lang_name)
-    if full_lang_name
+    if full_lang_name == 'ecmascript'
+      ts = super(code, 'typescript').sub("highlight typescript") do |match|
+        match + " tab-typescript"
+      end
+      js = super(code, 'javascript').sub("highlight javascript") do |match|
+        match + " tab-javascript"
+      end
+      ts + js
+    elsif full_lang_name == 'transpilescript'
+      # super('console.log("FoO");', 'javascript') 
+      ts = super(code, 'typescript').sub("highlight typescript") do |match|
+        match + " tab-typescript"
+      end
+
+      File.open('scrypted/typescript.ts', 'w') { |file| file.write(code) }
+      Dir.chdir('scrypted') {
+        system('tsc -t ES2018 -m ES6 typescript.ts')
+      }
+      jscode = File.read('scrypted/typescript.js')
+
+      js = super(jscode, 'javascript').sub("highlight javascript") do |match|
+        match + " tab-javascript"
+      end
+      ts + js
+    elsif full_lang_name
       parts = full_lang_name.split('--')
       rouge_lang_name = (parts) ? parts[0] : "" # just parts[0] here causes null ref exception when no language specified
       super(code, rouge_lang_name).sub("highlight #{rouge_lang_name}") do |match|
